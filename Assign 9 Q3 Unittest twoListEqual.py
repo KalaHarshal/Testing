de4511058b652c9a5324428ed178de607c7a9498
Harshal Kala <138870614+KalaHarshal@users.noreply.github.com>
2024-11-11 22:55:19 +0530

    Update Assign 9 Q3 Unittest twoListEqual.py

--- Assign 9 Q3 Unittest twoListEqual.py
+++ Assign 9 Q3 Unittest twoListEqual.py
@@ -1,15 +1,15 @@
 import unittest
 
-    def listEqualCheck(l1,l2):
+def listEqualCheck(l1,l2):
         if l1==l2:
             return True
         else:
             return False
 
-    class listEqCheck(unittest.TestCase):
+class listEqCheck(unittest.TestCase):
 
-        def test_listEqCheck(self):
-            self.assertTrue(listEqualCheck([&#39;Yash&#39;,&#39;Sam&#39;],[&#39;Yash&#39;,&#39;Sam&#39;]))
+    def test_listEqCheck(self):
+        self.assertTrue(listEqualCheck(["Yash","Sam"],["Yash","Sam"]))
 
-if __name__ == "__main__"
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
