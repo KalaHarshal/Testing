b46d68d4f1ce156c9883fbd817137c2cea9863ab
Harshal Kala <138870614+KalaHarshal@users.noreply.github.com>
2024-11-11 12:40:38 +0530

    Add files via upload

---
+++ Assign 6 Q2 filter odd.py
@@ -0,0 +1,8 @@
+# Input list of integers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Use filter with a lambda function to filter even numbers
+even_numbers = list(filter(lambda x: x % 2 !=0 , numbers))
+
+# Print the filtered list of even numbers
+print(even_numbers)
